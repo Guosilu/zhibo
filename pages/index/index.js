19e7d989351a3f0eeb861bbeaf24669a6d9bd010
wjm <13156007572@163.com>
2018-10-31 09:59:42 +0800

    上拉触底删除

--- pages/index/index.js
+++ pages/index/index.js
@@ -132,37 +132,9 @@ Page({
      * 页面上拉触底事件的处理函数
      */
   onReachBottom: function () {
-    var that = this;
-    // 显示加载图标
-    wx.showLoading({
-      title: '玩命加载中',
-    })
-    // 页数+1
-    page = page + 1;
-    wx.request({
-      url: 'https://aa.zdcom.net.cn/' + page,
-      method: "GET",
-      // 请求头部
-      header: {
-        'content-type': 'application/text'
-      },
-      success: function (res) {
-        // 回调函数
-        var moment_list = that.data.moment;
-
-        for (var i = 0; i < res.data.data.length; i++) {
-          moment_list.push(res.data.data[i]);
-        }
-        // 设置数据
-        that.setData({
-          moment: that.data.moment
-        })
-        // 隐藏加载框
-        wx.hideLoading();
-      }
-    })
 
   },
+
   /**
    * 用户点击右上角分享
    */
