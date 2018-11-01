431885ab7778008e2a0f8d1915219a294e8341b6
liupengcheng <2212999068@qq.com>
2018-11-01 17:30:43 +0800

    111

--- pages/show/show.js
+++ pages/show/show.js
@@ -1,7 +1,18 @@
+
+function getRandomColor() {
+  let rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length == 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+
 Page({
-  inputValue: '',
+inputValue: '',
   data: {
-    src: '',
+  src: '',
     danmuList: [
       {
         text: '第 1s 出现的弹幕',
@@ -13,8 +24,61 @@ Page({
         color: '#ff00ff',
         time: 3
       }]
-  }, 
-  
+},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  onReady: function (res) {
+    this.videoContext = wx.createVideoContext('myVideo')
+  },
+
   getRandomColor: function () {
     let rgb = []
     for (let i = 0; i < 3; ++i) {
@@ -32,19 +96,19 @@ Page({
   bindInputBlur: function (e) {
     this.inputValue = e.detail.value
   },
-  bindButtonTap: function () {
-    var that = this
-    wx.chooseVideo({
-      sourceType: ['album', 'camera'],
-      maxDuration: 60,
-      camera: ['front', 'back'],
-      success: function (res) {
-        that.setData({
-          src: res.tempFilePath
-        })
-      }
-    })
-  },
+  // bindButtonTap: function () {
+  //   var that = this
+  //   wx.chooseVideo({
+  //     sourceType: ['album', 'camera'],
+  //     maxDuration: 60,
+  //     camera: ['front', 'back'],
+  //     success: function (res) {
+  //       that.setData({
+  //         src: res.tempFilePath
+  //       })
+  //     }
+  //   })
+  // },
   bindSendDanmu: function () {
     this.videoContext.sendDanmu({
       text: this.inputValue,
