7ac1beb8bd4ba60d629ba51bf6f36eb89d240417
unknown <unknown@example.com>
2018-11-04 13:38:03 +0800

    1

--- js/uploadFun.js
+++ js/uploadFun.js
@@ -1,3 +1,9 @@
+
+/** 
+ * 单文件上传
+ * paramObj: {url: '', filePath: '', formData: '', name: '', columnName: ''}
+ * resol: {columnName: '', fileUrl: ''}
+*/
 function fileUpload(paramObj) {
   return new Promise(function (resolve, reject) {
     wx.uploadFile({
@@ -8,12 +14,12 @@ function fileUpload(paramObj) {
       success: function (res) {
         let data = JSON.parse(res.data);
         let success = data.success;
-        console.log(data);
         if (success == 1) {
           let resol = {
             columnName: paramObj.columnName,
             fileUrl: data.file_url,
           }
+          console.log(resol);
           resolve(resol);
         } else {
           resolve(0);
@@ -23,7 +29,14 @@ function fileUpload(paramObj) {
   });
 }
 
-function uploadFileNameList (paramObjList) {
+/** 
+ * 多文件上传
+ * paramObjList: {paramObj1[, paramObj2 ,...]}
+ * fileNameList: [{columnName: '', fileUrl: ''}, {} ...] / {{columnName1: fileUrl1}, {}, ...}
+ * objType: array, json
+*/
+function uploadFileNameList (paramObjList, objType) {
+  var objType = objType || "array";
   return new Promise(function (resolve, reject) {
     let promiseArr = [];
     for (let i = 0; i < paramObjList.length; i++) {
@@ -31,16 +44,25 @@ function uploadFileNameList (paramObjList) {
       promiseArr.push(promise);
     }
     Promise.all(promiseArr).then(res => {
-      let fileNameList = [];
-      for (let i = 0; i < res.length; i++) {
-        //fileNameList[res[i].columnName] = res[i].fileUrl
-        let fileNameList1 = {
-          columnName: res[i].columnName,
-          fileUrl: res[i].fileUrl
+      if (objType == "array") {
+        let fileNameList = [];
+        for (let i = 0; i < res.length; i++) {
+          let fileNameOne = {
+            columnName: res[i].columnName,
+            fileUrl: res[i].fileUrl
+          }
+          fileNameList.push(fileNameOne);
         }
-        fileNameList.push(fileNameList1);
+        console.log(fileNameList);
+        resolve(fileNameList);
+      } else if (objType == "json") {
+        let fileNameList = {};
+        for (let i = 0; i < res.length; i++) {
+          fileNameList[res[i].columnName] = res[i].fileUrl;
+        }
+        console.log(fileNameList);
+        resolve(fileNameList);
       }
-      resolve(fileNameList);
     });
   })
 }
