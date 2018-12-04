a480d8cc545afcb8d0d3bc69438448cb03f0ee24
liupengcheng <2212999068@qq.com>
2018-12-04 14:30:22 +0800

    12121

--- pages/list/list.js
+++ pages/list/list.js
@@ -6,12 +6,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    limit: 4,//显示数据量
-    allList: [],
-    page: 1,
-    pagesize: 10,
+    limit: 4,//显示数据量 
+    loadingComplate: 0,
+    allList: []
   },
 
+  //生命周期函数--监听页面加载
+  onLoad: function (options) {
+    this.showLoading('正在加载...');
+    this.getList();
+  },
 
   //列表
   getList: function () {
@@ -47,55 +51,11 @@ Page({
         allList: res
       });
       that.stopRefresh();
+      that.setData({
+        loadingComplate: 1
+      })
       console.log(res);
     });
-  },
-
-  getList1: function (refresh) {
-    var that = this;
-    var refresh = refresh || '';
-    if (refresh == 1) {
-      var page = 1;
-      var pagesize = this.data.pagesize;
-    } else {
-      var page = this.data.page;
-      var pagesize = this.data.pagesize;
-    }
-    wx.request({
-      url: config.coreUrl + 'getRoom.php',
-      method: 'POST',
-      dataType: 'json',
-      data: {
-        action: "list",
-        order: '`start_time` DESC',
-        page: page,
-        pagesize: pagesize,
-      },
-      success: function (res) {
-        if (res.data.length > 0) {
-          if (refresh == 1) {
-            that.setData({
-              list: res.data,
-              page: 1,
-            })
-          } else {
-            that.setData({
-              list: that.data.list.concat(res.data),
-              page: page + 1,
-            })
-          }
-          console.log(res.data);
-        } else {
-          wx.showToast({
-            icon: 'none',
-            title: '到底了~',
-          })
-        }
-      },
-      complete: function () {
-        that.stopRefresh();
-      }
-    })
   },
 
   stopRefresh: function () {
@@ -103,28 +63,38 @@ Page({
     wx.hideNavigationBarLoading() //完成停止加载
     wx.stopPullDownRefresh() //停止下拉刷新
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    wx.showLoading({
-      title: '正在加载...',
-    })
-    this.getList();
-  },
+
   /**
   * 页面相关事件处理函数--监听用户下拉动作
   */
   onPullDownRefresh: function () {
     wx.showNavigationBarLoading() //在标题栏中显示加载
-    this.getList(1);
+    this.getList();
   },
+
+  //提示方法
+  showTip: function (msg) {
+    wx.showToast({
+      icon: 'none',
+      title: msg,
+    })
+  },
+
+  //加载方法
+  showLoading: function (msg, mask) {
+    var mask = mask || false;
+    wx.showLoading({
+      mask: mask,
+      title: msg,
+    })
+  },
+
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    this.getList();
   },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
